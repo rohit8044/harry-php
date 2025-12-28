2a3b0a6ec0bd07cfcbe7b3d6c06764c9c1caf85a
rohit8044 <trickcric2@gmail.com>
2025-12-28 06:26:42 -0800

    Add files via upload

---
+++ src/Loginpage.jsx
@@ -0,0 +1,32 @@
+import './Loginpage.css'
+import { useNavigate } from 'react-router-dom'
+export default function Loginpage(){
+   const navigate = useNavigate();
+    const submit=(e)=>{
+        e.preventDefault();
+        navigate("/dashbord")
+    }
+    return(
+        <>
+        <div className="container">
+            <div className='logindiv'>
+                <form onSubmit={submit} > 
+                <label>Phone Nubmer</label>
+                <div className='inputbox'>
+                    <span className="logo">📞</span>
+                    <input type='text' placeholder='Phone Nubmer'></input>
+                </div>
+                <label>Password</label>
+                 <div  className='inputbox'>
+                    <span className="logo">🔒</span>
+                    <input type='text' placeholder='Password'></input>
+                </div>
+                <button className='loginbtn'>Login</button>
+                <a href="register" className='create'>Creact Account</a>
+                <a href="#" className='forget'>Forget Password</a>
+                </form>
+            </div>
+        </div>
+        </>
+    )
+}
---
+++ src/Phone.jsx
@@ -0,0 +1,18 @@
+import './Phone.css'
+export default function Phone(){
+    return(
+        <>
+        <div className="hii"> 
+            <h2 className='headtext'>FF HACK DOWNLOAD</h2>
+        <div className='prent'> 
+             <div className='box'>
+                <img src='https://s.cafebazaar.ir/images/icons/com.fff_ffh4xforfire.ff_modmenuhack-d7bd90fc-cdc9-495c-a1df-873ae6512386_512x512.png?x-img=v1/resize,h_256,w_256,lossless_false/optimize'></img>
+                <p className='hackname'>Hg Cheat</p>
+                   <div className='hackbutton'> <a href='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTG4j-7QwDZX-yq98DtWIR1ow_UK_Clg8wm8aqbEd7DqO-Qjk_iedtH-N0HxfF7MQOzDJg&usqp=CAU' className='acha' download> DOWNLOAD </a> </div>
+                </div> 
+             <div className='box'></div>
+        </div>
+        </div>
+        </>
+    )
+}
